3334d2fb1358a2a03f000c802a4274d929b01254
Laurel Thorburn <laurelthorburn@gmail.com>
2021-12-04 10:31:59 -0700

    Got initial login route to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const { User, Post } = require("./../models");
 
 // -------------------------------------------------------------
 
-// GET all posts
+// GET initial page
 router.get("/", async (req, res) => {
   console.log("Did you make it to me?? 1");
   try {
@@ -25,6 +25,39 @@ router.get("/", async (req, res) => {
 
       // Pass serialized data and session flag into template
     res.render("all", {
+      posts,
+      username: req.session.username,
+      user_id: req.session.user_id,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// -------------------------------------------------------------
+
+// GET all posts
+router.get("/dashboard", async (req, res) => {
+  console.log("Did you make it to me?? 1");
+  try {
+    console.log("Did you make it to me?? 2");
+
+    // Get all Posts and JOIN with User data
+    const dbPostData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+    console.log("Did you make it to me?? 3");
+
+      // Serialize data so the template can read it -- else you get a mass of information that is overwhelming and difficult to work with
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+      // Pass serialized data and session flag into template
+    res.render("dashboard", {
       posts,
       username: req.session.username,
       user_id: req.session.user_id,
