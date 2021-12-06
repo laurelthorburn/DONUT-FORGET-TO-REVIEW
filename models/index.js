7d5bd402d59fef2f98aabba6142398b11103c5ba
Flor C <florenciabelenc94@gmail.com>
2021-12-06 16:45:39 -0700

    Started Cloudinary server

--- models/index.js
+++ models/index.js
@@ -3,23 +3,23 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 Post.belongsTo(User, {
-    foreingKey:'user_id'
+    foreignKey: 'user_id'
 });
 
 User.hasMany(Post, {
-    foreignKey:'user_id',
-    onDelete:'CASCADE'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
     foreignKey: 'user_id',
-  });
-  
-User.hasMany(Comment, {
-foreignKey: 'user_id',
 });
 
- Post.hasMany(Comment, {
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
     foreignKey: 'post_id',
 });
 
