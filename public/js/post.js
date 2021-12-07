2c48f98a02a00a3cfd3a74b21dd2cc29e658a7c1
Laurel Thorburn <laurelthorburn@gmail.com>
2021-12-06 20:11:58 -0700

    Created add new review modal

--- public/js/post.js
+++ public/js/post.js
@@ -87,6 +87,17 @@ const post_title = document.querySelector('#recipient-name').value;
     }
   }
 };
+
+// slider
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
  
  document
    .querySelector('.new-post-form')
