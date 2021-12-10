6f4e3d327be48275c7d970108abdc0dcc4dbbb11
Laurel Thorburn <laurelthorburn@gmail.com>
2021-12-09 18:43:46 -0700

    Working on favorites

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const favoriteRoutes = require('./favoriteRoutes');
 
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/favorites', favoriteRoutes);
 
 
 module.exports = router;
