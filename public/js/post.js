384b981696337e40f68f712e8aa24a85e5435ddd
Laurel Thorburn <laurelthorburn@gmail.com>
2021-12-11 08:45:55 -0700

    Added modal to profile page

--- public/js/post.js
+++ public/js/post.js
@@ -1,29 +1,45 @@
-let postID;
 
-const newFormHandler = async (event) => {
-   event.preventDefault();
- 
-   const post_title = document.querySelector('#post-name').value;
-   const post_content = document.querySelector('#post-desc').value;
- 
-   if (post_title && post_content) {
-     const response = await fetch(`/api/posts`, {
-       method: 'POST',
-       body: JSON.stringify({ post_title, post_content }),
-       headers: {
-         'Content-Type': 'application/json',
-       },
-     });
- 
-     if (response.ok) {
-       document.location.replace('/profile');
-     } else {
-       alert('Failed to create post');
+// slider
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("rating-num");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+const newReviewButton = async (event) => {
+    //  event.preventDefault();
+     console.log("This is a test. Do we make it here from the modal?");
+   
+     const post_title = document.querySelector('#post-name').value;
+     const post_content = document.querySelector('#post-desc').value;
+     const post_rating = slider.value;
+     const post_img = cloudURL;
+
+     console.log("I am the img link of the post... maybe??: ",
+     "post-img:",
+     post_img, //works
+     "=======");
+
+     if (post_title && post_content) { //do we want to force the user to upload an img?
+       const response = await fetch(`/api/posts`, {
+         method: 'POST',
+         body: JSON.stringify({ post_title, post_content, post_rating, post_img }),
+         headers: {
+           'Content-Type': 'application/json',
+         },
+       });
+   
+       if (response.ok) {
+         document.location.replace('/profile');
+       } else {
+         alert('Failed to create post');
+       }
      }
-   }
- };
- 
-
+   };
 
  const delButtonHandler = async (event) => {
    if (event.target.hasAttribute('data-id')) {
@@ -87,10 +103,14 @@ const post_title = document.querySelector('#recipient-name').value;
   }
 };
 
+
+document
+.querySelector('#upload-btn')
+.addEventListener('click', newReviewButton);
  
- document
-   .querySelector('.new-post-form')
-   .addEventListener('submit', newFormHandler);
+//  document
+//    .querySelector('.new-post-form')
+//    .addEventListener('submit', newFormHandler);
  
  document
    .querySelector('.post-list')
