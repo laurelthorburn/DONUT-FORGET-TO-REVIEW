d4f80d0c03e0819a8438f61033239baa167363b1
Laurel Thorburn <laurelthorburn@gmail.com>
2021-12-11 07:04:22 -0700

    Uploaded img from modal to cloudinary and back

--- public/js/reviewmodal.js
+++ public/js/reviewmodal.js
@@ -16,20 +16,17 @@ const newReviewButton = async (event) => {
      const post_title = document.querySelector('#post-name').value;
      const post_content = document.querySelector('#post-desc').value;
      const post_rating = slider.value;
-    //  const post_img = cloudURL;
-     console.log("I am the rating of the post... maybe??: ",
-     "post-rating:",
-     post_rating, 
-     "=======",
-     "output",
-     output, 
-     "slider.value",
-     slider.value);
+     const post_img = cloudURL;
 
-     if (post_title && post_content) {
+     console.log("I am the img link of the post... maybe??: ",
+     "post-img:",
+     post_img, //works
+     "=======");
+
+     if (post_title && post_content) { //do we want to force the user to upload an img?
        const response = await fetch(`/api/posts`, {
          method: 'POST',
-         body: JSON.stringify({ post_title, post_content, post_rating }),
+         body: JSON.stringify({ post_title, post_content, post_rating, post_img }),
          headers: {
            'Content-Type': 'application/json',
          },
@@ -37,7 +34,6 @@ const newReviewButton = async (event) => {
    
        if (response.ok) {
          document.location.replace('/dashboard');
-        console.log("HI");
        } else {
          alert('Failed to create post');
        }
